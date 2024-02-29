053dd73a2e8693a9da3550ceb72d96e3db6035d3
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-02-29 15:06:31 -0500

    finished about section, needs text and new image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
+import About from "./components/About";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Skills />
+      <About />
     </div>
   );
 }
