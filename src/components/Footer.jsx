8e512966d3d60654d5c4ac313cd80fd5ba1e6d2b
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-02-29 21:40:42 -0500

    added footer and downloadable CV

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="border-t border-gray-600 max-w-[1200px] sm:h-[150px] p-12 flex justify-between mx-auto">
+        <span className="primary-color">Jonathan Battista</span>
+        <p className="text-gray-600">
+          linkedin.com/in/jonathan-battista <br /> jonathanbattista1@gmail.com
+        </p>
+      </div>
+      <p className="text-gray-600 mt-2 flex justify-center text-xs">
+        @ Jonathan. All Rights Reserved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
