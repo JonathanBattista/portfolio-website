81933ac6548ab3ecba27a417db32bfaaa69811b0
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-03-01 00:33:32 -0500

    added work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,74 @@
 import React from "react";
+import critic from "../assets/critic-consolidator.png";
+import socketio from "../assets/socket-io.jpg";
+import cs2 from "../assets/project2.jpeg";
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className="max-w-[1200px] mx-auto p-5" id="work">
+      <div className="pb-8">
+        <p className="text-4xl mb-3 font-bold primary-color">Work</p>
+        <p className="text-gray-400">Check out some of my recent work</p>
+      </div>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center h-[200px] bg-cover relative">
+          <img
+            src={critic}
+            alt="critic consolidator"
+            layout="fill"
+            objectfit="cover"
+          />
+          <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
+            <span className="text-2xl font-bold text-white tracking-wider">
+              Critic Consolidator
+            </span>
+            <div className="pt-8 text-center">
+              <a href="/">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  Live
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center h-[200px] bg-cover relative">
+          <img src={socketio} alt="socket.io" layout="fill" objectfit="cover" />
+          <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
+            <span className="text-2xl font-bold text-white tracking-wider">
+              Socket.io tutorial
+            </span>
+            <div className="pt-8 text-center">
+              <a href="/">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  Live
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center h-[200px] bg-cover relative">
+          <img
+            src={cs2}
+            alt="FPS 10-man website"
+            layout="fill"
+            objectfit="cover"
+          />
+          <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
+            <span className="text-2xl font-bold text-white tracking-wider">
+              FPS 10-man website
+            </span>
+            <div className="pt-8 text-center">
+              <a href="/">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  Soon
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Work;
