09ef9eee341cba37502fc046e08de292cb7e2aa4
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-05-12 15:40:13 -0400

    added new proficient skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,10 +10,13 @@ import sass from "../assets/sass.png";
 import express from "../assets/express.png";
 import mysql from "../assets/mysql.png";
 import jest from "../assets/jest.png";
+import typeScript from "../assets/typescript-icon.png";
+import mongoDB from "../assets/mongodb-plain-wordmark.png";
+import elasticSearch from "../assets/icons8-elasticsearch-480.png";
 
 const Skills = () => {
   return (
-    <div className="border border-gray-600 bg-black text-gray-400  max-w-[1200px] mx-auto grid grid-cols-6 place-items-center">
+    <div className="border border-gray-600 bg-black text-gray-400  max-w-[1200px] mx-auto grid grid-cols-5 place-items-center">
       <h2 className="primary-color text-xl md:text-4xl font-bold m-4">
         My <br /> Tech <br /> Stack
       </h2>
@@ -60,6 +63,18 @@ const Skills = () => {
       <div className="flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]">
         <img src={jest} width={100} height={100} alt="" />
         <p className="mt-2">Jest</p>
+      </div>
+      <div className="flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]">
+        <img src={typeScript} width={100} height={100} alt="" />
+        <p className="mt-2">TypeScript</p>
+      </div>
+      <div className="flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]">
+        <img src={mongoDB} width={100} height={100} alt="" />
+        <p className="mt-2">MongoDB</p>
+      </div>
+      <div className="flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]">
+        <img src={elasticSearch} width={100} height={100} alt="" />
+        <p className="mt-2">Elasticsearch</p>
       </div>
     </div>
   );
