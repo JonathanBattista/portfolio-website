9f7fc329e1b0bec59a433b4c49df4397b1be335c
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-09-30 10:34:53 -0400

    added new website to work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import critic from "../assets/critic-consolidator.png";
 import socketio from "../assets/socket-io.jpg";
 import cs2 from "../assets/project2.jpeg";
+import EvCharger from "../assets/Untitled design (1).png";
 
 const Work = () => {
   return (
@@ -11,6 +12,26 @@ const Work = () => {
         <p className="text-gray-400">Check out some of my recent work</p>
       </div>
       <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center h-[200px] bg-cover relative">
+          <img
+            src={EvCharger}
+            alt="EV Charger Dashboard"
+            layout="fill"
+            objectfit="cover"
+          />
+          <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
+            <span className="text-2xl font-bold text-white tracking-wider">
+              EV Charger Dashboard
+            </span>
+            <div className="pt-8 text-center">
+              <a href="/">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  Private Freelance Project
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
         <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center h-[200px] bg-cover relative">
           <img
             src={critic}
